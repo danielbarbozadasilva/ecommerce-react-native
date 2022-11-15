6470f29dd9ec6a979791e6cc0f8e91f9778c0ac5
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-15 18:30:45 -0300

    feature: create signin screen

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container} from './styled';
+import {Text} from 'react-native';
+
+const SignIn = () => {
+  <Container>
+    <Text>SignIn</Text>
+  </Container>;
+};
+export default SignIn;
