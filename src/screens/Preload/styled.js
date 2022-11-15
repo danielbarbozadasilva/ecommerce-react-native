b6eef122f7b590d0b4688c55dc75c78b679b1e14
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-15 18:30:27 -0300

    feature: create preload screen

---
+++ src/screens/Preload/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  background-color: #fff;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+export const LoadingIcons = styled.ActivityIndicator``
