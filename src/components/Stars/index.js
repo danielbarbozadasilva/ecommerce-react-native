29d17886d43ccf1bc9f08bd6e90cf3a38f0808c2
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-27 12:23:55 -0300

    feat: create star component

---
+++ src/components/Stars/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import StarFull from '../../assets/svg/star.svg';
+import StarHalf from '../../assets/svg/star_half.svg';
+import StarEmpty from '../../assets/svg/star_empty.svg';
+import {StarArea, StarView, StarText} from './styled';
+
+const Stars = ({stars, showNumber}) => {
+  let s = [0, 0, 0, 0, 0];
+  let floor = Math.floor(stars);
+  let left = stars - floor;
+
+  for (var i = 0; i < floor; i++) {
+    s[i] = 2;
+  }
+  if (left > 0) {
+    s[i] = 1;
+  }
+
+  return (
+    <StarArea>
+      {s.map((i, k) => (
+        <StarView key={k}>
+          {i === 0 && <StarEmpty width="18" height="18" fill="#FF9200" />}
+          {i === 1 && <StarHalf width="18" height="18" fill="#FF9200" />}
+          {i === 2 && <StarFull width="18" height="18" fill="#FF9200" />}
+        </StarView>
+      ))}
+      {showNumber && <StarText>{stars}</StarText>}
+    </StarArea>
+  );
+};
+
+export default Stars;
