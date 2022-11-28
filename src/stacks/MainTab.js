361197cd59b26d9512df56916f10ae4163730da6
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-28 09:01:05 -0300

    refactor: main tab

--- src/stacks/MainTab.js
+++ src/stacks/MainTab.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
-import CustomTabBar from '../components/CustomTabBar';
-
-import Home from '../screens/Home';
-import Search from '../screens/Search';
-import Favorites from '../screens/Favorites';
-import Profile from '../screens/Profile';
+import CustomTabBar from '../components/CustomTabBar/index';
+import Home from '../screens/Home/index';
+import Categorias from '../screens/Categories/index';
+import Carrinho from '../screens/Cart/index';
+import Favoritos from '../screens/Favorites/index';
+import Perfil from '../screens/Profile/index';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,11 +13,12 @@ const MainTab = () => {
   return (
     <Tab.Navigator tabBar={props => <CustomTabBar {...props} />}>
       <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Favorites" component={Favorites} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Categorias" component={Categorias} />
+      <Tab.Screen name="Carrinho" component={Carrinho} />
+      <Tab.Screen name="Favoritos" component={Favoritos} />
+      <Tab.Screen name="Perfil" component={Perfil} />
     </Tab.Navigator>
   );
 };
 
-export default MainTab
+export default MainTab;
