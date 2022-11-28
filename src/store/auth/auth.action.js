43c10d46c36e32c35e3cea5e2636b82ea26e91fa
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-11-28 08:59:04 -0300

    refactor: clean and optimize code

--- src/store/auth/auth.action.js
+++ src/store/auth/auth.action.js
@@ -1,10 +1,23 @@
+import TYPES from '../types';
 import http from '../../config/http';
+import {setStorageItem} from '../../config/auth';
+import {Alert} from 'react-native';
 
-export const signInAction = async data => {
-  try {
-    const result = await http.post('/auth', data);
-    return result.data;
-  } catch (error) {}
+export const signInAction = data => {
+  return async dispatch => {
+    dispatch({type: TYPES.AUTH_LOADING, status: true});
+    try {
+      const result = await http.post('/auth', data);
+      if (result?.data?.data?.token) {
+        await setStorageItem('token', result.data?.data.token);
+        dispatch({type: TYPES.SIGN_IN, data: result.data?.data.token});
+      }
+    } catch (error) {
+      const {data} = error.response;
+      Alert.alert('Erro', data.message);
+      dispatch({type: TYPES.SIGN_ERROR, data: error});
+    }
+  };
 };
 
 export const checkTokenAction = async data => {
@@ -14,23 +27,35 @@ export const checkTokenAction = async data => {
   } catch (error) {}
 };
 
-export const sendTokenAction = async data => {
-  try {
-    const result = await http.put('/user/recovery/password-recovery', data);
-    return result.data;
-  } catch (error) {}
+export const sendTokenAction = data => {
+  return async dispatch => {
+    dispatch({type: TYPES.AUTH_LOADING, status: true});
+    try {
+      const result = await http.put('/user/recovery/password-recovery', data);
+      dispatch({type: TYPES.AUTH_TOKEN, data: result.data.data});
+      return result.data;
+    } catch (error) {}
+  };
 };
 
-export const recoveryPasswordAction = async data => {
-  try {
-    const result = await http.put('/user/recovery/reset-password', data);
-    return result.data;
-  } catch (error) {}
+export const recoveryPasswordAction = data => {
+  return async dispatch => {
+    dispatch({type: TYPES.AUTH_LOADING, status: true});
+    try {
+      const result = await http.put('/user/recovery/reset-password', data);
+      dispatch({type: TYPES.AUTH_TOKEN, data: result.data.data});
+      return result.data;
+    } catch (error) {}
+  };
 };
 
-export const signUpAction = async data => {
-  try {
-    const result = await http.post('/client', data);
-    return result.data;
-  } catch (error) {}
+export const signUpAction = data => {
+  return async dispatch => {
+    dispatch({type: TYPES.AUTH_LOADING, status: true});
+    try {
+      const result = await http.post('/client', data);
+      dispatch({type: TYPES.PRODUCT_LIST, data: result.data.data});
+      return result.data;
+    } catch (error) {}
+  };
 };
--- src/store/auth/auth.reducer.js
+++ src/store/auth/auth.reducer.js
@@ -1,12 +1,46 @@
-export const initialState = {
-  avatar: '',
+import TYPES from '../types';
+
+const INITIAL_STATE = {
+  loading: false,
+  error: [],
+  registered: false,
 };
 
-export const userReducers = (state, action) => {
+const reducer = ({...state} = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'setAvatar':
-      return {...state, avatar: action.payload.avatar};
+    case TYPES.AUTH_LOADING:
+      state.error = [];
+      state.loading = action.status;
+      return state;
+
+    case TYPES.SIGN_IN:
+      state.registered = true;
+      state.token = action.data.token;
+      state.user = action.data.userDTO;
+      state.loading = false;
+      return state;
+
+    case TYPES.SIGN_UP:
+      state.registered = true;
+      state.token = action.data.token;
+      state.user = action.data.userDTO;
+      state.loading = false;
+      return state;
+
+    case TYPES.SIGN_ERROR:
+      const err = [...state.error, action.data];
+      state.loading = false;
+      state.error = err;
+      return state;
+
+    case TYPES.SIGN_OUT:
+      state.token = '';
+      state.user = {};
+      state.error = [];
+
     default:
       return state;
   }
 };
+
+export default reducer;
