eb81d00b4ef48c7a14b969dbcc3c67f3dafa3c7f
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-04 20:04:46 -0300

    feat: ensure the listing of products by their id

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,5 +1,6 @@
 import http from '../config/http';
 
 export const getProductsService = () => http.get('/product');
+export const getProductByIdService = id => http.get(`/product/${id}`);
 export const getProductsSearchService = name =>
   http.get(`/product/search/${name}`);
