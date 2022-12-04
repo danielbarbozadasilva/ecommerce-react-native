5956eb645ed23b588b1f7d80cc5f441fea342253
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-04 14:23:30 -0300

    feat: create loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {LoadingIcon} from './styled';
+
+const Loading = () => {
+  return <LoadingIcon />;
+};
+
+export default Loading;
---
+++ src/components/Loading/styled.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
+`;
