1fb0d35bc73ebaba5e70df52d54cf604e0684d5c
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-12-09 18:41:41 -0300

    feat: ensure decode token

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {API_TOKEN_KEY} from '@env';
 const baseKey = API_TOKEN_KEY;
+import jwt_decode from 'jwt-decode';
 
 export const setStorageItem = async (key, value) => {
   await AsyncStorage.setItem(`${baseKey}${key}`, value);
@@ -12,4 +13,8 @@ export const getStorageItem = async key => {
 
 export const removeStorageItem = async key => {
   await AsyncStorage.removeItem(`${baseKey}${key}`);
+};
+
+export const decodeToken = async (key = 'token') => {
+  return jwt_decode(await AsyncStorage.getItem(`${baseKey}${key}`));
 };
